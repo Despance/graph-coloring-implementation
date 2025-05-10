d58304fda35b2fd6b857fc916ae7872462866be8
MuhammedHayta <104199871+MuhammedHayta@users.noreply.github.com>
2025-05-10 17:45:47 +0300

    tried our heuristic with rlf, nothing good came out of it

--- rlf_color.c
+++ rlf_color.c
@@ -9,7 +9,10 @@
 // Function prototypes
 void rlfColor(int **graph, int nodes);
 void standartRLFColor(int **graph, int nodes, int *solution, int *orderOfNodes, bool *colored);
+void importanceRLFColor(int **graph, int nodes, int *solution, int *orderOfNodes, bool *colored);
+void colorMaxIndependentSetImportance(int **graph, int nodes, int *solution, int *orderOfNodes, bool *colored, int currentNode, int *nodeWeights);
 void colorMaxIndependentSet(int **graph, int nodes, int *solution, int *orderOfNodes, bool *colored, int currentNode);
+int findMaxIndependentNodeImportance(int **graph, int nodes, bool *colored, bool *currentSet, int *nodeWeights);
 int findMaxIndependentNode(int **graph, int nodes, bool *colored, bool *currentSet);
 int getNumOfNeighboursInSet(int **graph, int nodes, bool *currentSet, int currentNode);
 int getNumOfNeighboursNotInSet(int **graph, int nodes, bool *currentSet, int currentNode);
@@ -18,6 +21,10 @@ int getNumOfNeighbours(int **graph, int nodes, int currentNode);
 int findNextAvailableColor(int *solution, int nodes, bool *currentSet);
 void removeNode(int **graph, int nodes, int node);
 
+// Function prototypes for functions from imp_color.c, these should be in a seperate file in the future
+void updateNodeWeights(int **graph, int *nodeWeights, int nodes, int n);
+int findHighestWeightedNode(int *nodeWeights, bool *colored, int nodes);
+void resetWeights(int **graph, int *nodeWeights, int nodes);
 
 int *solution; // Solution array
 
@@ -50,7 +57,7 @@ void rlfColor(int **graph, int nodes){
     
     // Call the coloring function
     standartRLFColor(tempGraph, nodes, solution, orderOfNodes, colored);
-    
+    // importanceRLFColor(tempGraph, nodes, solution, orderOfNodes, colored); // Pass NULL for nodeWeights if not used
 
     // Free allocated memory
     free(colored);
@@ -84,6 +91,72 @@ void standartRLFColor(int **graph, int nodes, int *solution, int *orderOfNodes, 
         colorMaxIndependentSet(graph, nodes, solution, orderOfNodes, colored, currentNode);
         
     }
+
+}
+
+void importanceRLFColor(int **graph, int nodes, int *solution, int *orderOfNodes, bool *colored){
+    
+    // if node with largest num of neighbours can not be found(method returns -1), stop coloring
+    int currentNode = 0;
+
+    int *nodeWeights = (int *)calloc(nodes, sizeof(int)); // Node weights array
+    updateNodeWeights(graph, nodeWeights, nodes, 2); // Update node weights
+    while(true){
+        
+        
+        // STEP 1: Find the node with the highest weight as a starting point(this is the modified version)
+        currentNode = getNodeWithLargesNumberOfNeighbours(graph, nodes, colored);
+
+        resetWeights(graph, nodeWeights, nodes); // Reset the node weights
+        updateNodeWeights(graph, nodeWeights, nodes, 2); // Update node weights
+        //currentNode = findHighestWeightedNode(nodeWeights, colored, nodes);
+        
+        if(currentNode == -1){
+            break; // No more uncolored nodes
+        }
+
+        // STEP 2: find the maximum independent set in the graph. then color and remove these nodes
+        colorMaxIndependentSet(graph, nodes, solution, orderOfNodes, colored, currentNode);
+        //colorMaxIndependentSetImportance(graph, nodes, solution, orderOfNodes, colored, currentNode, nodeWeights);
+        
+    }
+}
+
+void colorMaxIndependentSetImportance(int **graph, int nodes, int *solution, int *orderOfNodes, bool *colored, int currentNode, int *nodeWeights){
+    // init the current set S as an array with calloc (TODO: in future use the same array just memset it to false)
+    bool *currentSet = (bool *)calloc(nodes, sizeof(bool));
+    currentSet[currentNode] = true; // add the current node to the set S
+    
+
+    // do this until no more nodes can be added to the set S
+    while(true){
+
+        // call findMaxIndependentNode, add it to the set S
+        // int current = findMaxIndependentNode(graph, nodes, colored, currentSet); // original version
+         int current = findMaxIndependentNodeImportance(graph, nodes, colored, currentSet, nodeWeights); // use the importance version
+        if(current == -1){
+            break; // No more uncolored nodes
+        }
+        // add the node to order of Nodes
+        orderOfNodes[orderOfNodesIndex] = currentNode;
+        orderOfNodesIndex++;
+        currentSet[current] = true; // add the node to the set S
+    }
+    
+    // color the nodes in set S
+    
+    int color = findNextAvailableColor(solution, nodes, currentSet);
+    for(int i = 0; i < nodes; i++){
+        if(currentSet[i]){
+            solution[i] = color; // color the node
+            colored[i] = true; // mark the node as colored
+            // remove the nodes in set S from the graph(graph should be temporary)
+            removeNode(graph, nodes, i); // remove the node from the graph
+        }
+    }
+    
+
+    free(currentSet); // free the current set S
 
 }
 
@@ -139,6 +212,82 @@ void colorMaxIndependentSet(int **graph, int nodes, int *solution, int *orderOfN
 
     free(currentSet); // free the current set S
 
+}
+
+// given graph should be a temporary graph(with removed nodes)
+// changed heuristic 2 to use weights of the nodes.
+int findMaxIndependentNodeImportance(int **graph, int nodes, bool *colored, bool *currentSet, int *nodeWeights){
+
+    // TODO: instead of allocating memory for this array every time, we can just reuse it
+    // memset it to false
+    bool *neighboursOfCurrentSet = (bool *)calloc(nodes, sizeof(bool)); 
+    for(int i = 0; i < nodes; i++){
+        if(currentSet[i]){
+            for(int j = 0; j < nodes; j++){
+                if(graph[i][j] > 0){
+                    neighboursOfCurrentSet[j] = true;
+                }
+            }
+        }
+    }
+
+    int maxNeighbours = 0;
+    int maxUnCommon = 0x7FFFFFFF; // init to largest value
+    bool isMaxUnCommonSet = true;
+    int nodeWithMaxNeighbours = -1;
+    for(int i = 0; i < nodes; i++){
+        if(colored[i] || neighboursOfCurrentSet[i] || currentSet[i]){
+            continue; // Skip colored nodes
+        }
+        int current = getNumOfNeighboursInSet(graph, nodes, neighboursOfCurrentSet, i);
+        // if the current node has more common neighbours than the max, update the max
+        if(current > maxNeighbours){
+            maxNeighbours = current;
+            nodeWithMaxNeighbours = i;
+            isMaxUnCommonSet = false;
+        }
+        else if(current == maxNeighbours){
+            int currentNodeWeight = nodeWeights[i];
+            int maxNodeWeight = nodeWeights[nodeWithMaxNeighbours];
+            if(currentNodeWeight > maxNodeWeight){
+                maxNeighbours = current;
+                nodeWithMaxNeighbours = i;
+                isMaxUnCommonSet = false;
+            }
+            else if(currentNodeWeight == maxNodeWeight){
+                int currentUnCommon = getNumOfNeighboursNotInSet(graph, nodes, neighboursOfCurrentSet, i);
+                if(!isMaxUnCommonSet){ // if max uncommon is not up to date, recompute  it
+                    maxUnCommon = getNumOfNeighboursNotInSet(graph, nodes, neighboursOfCurrentSet, nodeWithMaxNeighbours);
+                    isMaxUnCommonSet = true;
+                }
+                if(currentUnCommon < maxUnCommon){
+                    maxNeighbours = current;
+                    nodeWithMaxNeighbours = i;
+                    maxUnCommon = currentUnCommon;
+                }
+            }
+            
+        }
+        /*
+        // original tie breaker heuristic
+        else if(current == maxNeighbours){
+
+            int currentUnCommon = getNumOfNeighboursNotInSet(graph, nodes, neighboursOfCurrentSet, i);
+            if(!isMaxUnCommonSet){ // if max uncommon is not up to date, recompute  it
+                maxUnCommon = getNumOfNeighboursNotInSet(graph, nodes, neighboursOfCurrentSet, nodeWithMaxNeighbours);
+                isMaxUnCommonSet = true;
+            }
+            if(currentUnCommon < maxUnCommon){
+                maxNeighbours = current;
+                nodeWithMaxNeighbours = i;
+                maxUnCommon = currentUnCommon;
+            }
+        }
+        */
+    }
+
+    free(neighboursOfCurrentSet);
+    return nodeWithMaxNeighbours;
 }
 
 
@@ -301,6 +450,108 @@ void removeNode(int **graph, int nodes, int node)
     {
         graph[node][i] = 0;
         graph[i][node] = 0;
+    }
+}
+
+
+// TODO: HAVE THE FUNCTIONS BELOW THIS COMMENT IN A SEPERATE FILE
+
+void updateNodeWeights(int **graph, int *nodeWeights, int nodes, int n)
+{
+
+    int *nodeValues = (int *)calloc(nodes, sizeof(int));           // Node degree values (init with 0)
+    int **tempEdgeWeights = (int **)malloc(nodes * sizeof(int *)); // Temporary edge weights
+
+    // Allocate memory for tempEdgeWeights
+    for (int i = 0; i < nodes; i++)
+    {
+        tempEdgeWeights[i] = (int *)malloc(nodes * sizeof(int));
+    }
+
+    // Iteration loop
+    for (int iter = 0; iter < n; iter++)
+    {
+
+        // Step 1: Calculate node degree values or update with current node weights
+        for (int i = 0; i < nodes; i++)
+        {
+            nodeValues[i] = 0;
+            for (int j = 0; j < nodes; j++)
+            {
+                if (graph[i][j] > 0) // TODO: we may not need this check(if we don't have overflows etc.)
+                {
+                    nodeValues[i] += graph[i][j]; // Sum of edge weights connected to the node
+                }
+            }
+        }
+
+        // Step 2: Calculate edge weights and update adjacency matrix
+        for (int i = 0; i < nodes; i++)
+        {
+            for (int j = 0; j < nodes; j++)
+            {
+                if (graph[i][j] > 0) // Edge exists
+                {
+                    tempEdgeWeights[i][j] = nodeValues[i] + nodeValues[j]; // Calculate edge weight
+                    graph[i][j] = tempEdgeWeights[i][j];                   // Update adjacency matrix with edge weight
+                }
+            }
+        }
+
+        // Step 3: Calculate node weights
+        for (int i = 0; i < nodes; i++)
+        {
+            nodeWeights[i] = 0;
+            for (int j = 0; j < nodes; j++)
+            {
+                if (graph[i][j] > 0) // Edge exists (TODO: we may not need this check)
+                {
+                    nodeWeights[i] += graph[i][j];
+                }
+            }
+        }
+    }
+
+    // Free dynamically allocated memory
+    free(nodeValues);
+    for (int i = 0; i < nodes; i++)
+    {
+        free(tempEdgeWeights[i]);
+    }
+    free(tempEdgeWeights);
+}
+
+// Function to find the highest weighted uncolored node
+int findHighestWeightedNode(int *nodeWeights, bool *colored, int nodes)
+{
+    int maxWeight = -1, selectedNode = -1;
+    for (int i = 0; i < nodes; i++)
+    {
+
+        if (!colored[i] && nodeWeights[i] > maxWeight)
+        {
+            maxWeight = nodeWeights[i];
+            selectedNode = i;
+        }
+    }
+    return selectedNode;
+}
+
+// function to reset the node and edge weights
+void resetWeights(int **graph, int *nodeWeights, int nodes)
+{
+    // Reset the node weights
+    memset(nodeWeights, 0, nodes * sizeof(int));
+    // reset the edge weights
+    for (int i = 0; i < nodes; i++)
+    {
+        for (int j = 0; j < nodes; j++)
+        {
+            if (graph[i][j] > 0)
+            {
+                graph[i][j] = 1;
+            }
+        }
     }
 }
 
